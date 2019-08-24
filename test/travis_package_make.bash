dbf1b8fe8ace1f39227c91363ef78470d177f4f2
ink <g.gullwing@gmail.com>
2019-08-24 23:09:52 +0900

    fix travis init file(11)

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,7 +5,7 @@ sudo apt install python-catkin-tools
 
 #sync and make
 rsync -av ./ ~/catkin_ws/src/pimouse_ros/
-#sudo apt-get install protobuf-compiler
+sudo apt-get install protobuf-compiler
 #pip install install -U rosdep rosinstall_generator wstool rosinstall six vcstools
 
 #create Log Space
